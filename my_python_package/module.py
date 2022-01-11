ba4866b18960efe45121586f3087423a72ccf443
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2022-01-11 16:54:16 +0530

    Co-authored-by: Sriram Kannan <Marirs27@users.noreply.github.com>

--- my_python_package/module.py
+++ my_python_package/module.py
@@ -11,6 +11,8 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import time
 import cv2
+import mediapipe as mp
+from tkinter import *
 
 # YOLO Object tracking (yolo-folder-path,video,object): string,string,string
 def track_object(yolo_path,video_path,object):
@@ -85,4 +87,43 @@ def track_object(yolo_path,video_path,object):
         f.write(str(center)+"\n")
     f.write("WIDTH: "+str(f_width))
     f.write("\nHEIGHT: "+str(f_height))
+    f.close()
+
+def track_posture():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+    WIDTH = 600
+    HEIGHT = 600
+
+    capture = cv2.VideoCapture(0)
+    with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose :
+        while(capture.isOpened()) :
+            ret , frame = capture.read()
+
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            result = pose.process(image)
+
+            try :
+                landmarks = result.pose_landmarks.landmark
+                coordinates = {"Nose" : [WIDTH *landmarks[0].x,HEIGHT*landmarks[0].y] , "Left-Shoulder" : [WIDTH *landmarks[11].x,HEIGHT*landmarks[11].y] , "Right-Shoulder" : [WIDTH *landmarks[12].x,HEIGHT*landmarks[12].y] , "Left-Elbow" : [WIDTH *landmarks[13].x,HEIGHT*landmarks[13].y] , "Right-Elbow" : [WIDTH *landmarks[14].x,HEIGHT*landmarks[14].y] , "Left-Wrist" : [WIDTH *landmarks[15].x,HEIGHT*landmarks[15].y] , "Right-Wrist" : [WIDTH *landmarks[16].x,HEIGHT*landmarks[16].y], "Left-Hip" : [WIDTH *landmarks[23].x,HEIGHT*landmarks[23].y], "Right-Hip" : [WIDTH *landmarks[24].x,HEIGHT*landmarks[24].y], "Left-Knee" : [WIDTH *landmarks[25].x,HEIGHT*landmarks[25].y], "Right-Knee" : [WIDTH *landmarks[26].x,HEIGHT*landmarks[26].y], "Left-Ankle" : [WIDTH *landmarks[27].x,HEIGHT*landmarks[27].y], "Right-Ankle" : [WIDTH *landmarks[28].x,HEIGHT*landmarks[28].y] , "Left-Sword" : [0,0] , "Right-Sword" : [0,0]}
+            except :
+                pass
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(image,result.pose_landmarks,mp_pose.POSE_CONNECTIONS)
+
+            cv2.imshow("Mediapipe Feed" , image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q') :
+                break
+
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+    f = open("coordinates.txt", "a")
+    f.write(coordinates)
     f.close()
---
+++ test.py
@@ -0,0 +1,7 @@
+from my_python_package.module import *
+
+yolo_path = "./yolo-coco"
+video_path = "input.mp4"
+object = "person"
+
+track_object(yolo_path,video_path,object)
