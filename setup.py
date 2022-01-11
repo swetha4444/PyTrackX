40152ef44a5875ef5642038b57d7ad7ef5d73315
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2022-01-11 15:50:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'PytrackX',
+    packages = ['my_python_package'],
+    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A general python framework for visual object tracking.n',
+    author = 'Swetha Saseendran',
+    author_email = 'swethasaseendran4@gmail.com',
+    url = 'https://github.com/swetha4444/PyTrackX',
+    download_url = 'download link you saved',
+    keywords = ['yolo', 'mediapipe','object detection','object tracking'],
+    classifiers = [],
+)
